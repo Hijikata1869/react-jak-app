3fc683dac04a5933fd257b55bce33e599da1fa06
Hijikata1869 <53096397+Hijikata1869@users.noreply.github.com>
2021-04-14 19:33:01 +0900

    ChakraUIでGlobalなスタイルを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
-      <Button>ボタン</Button>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>テストテストテスト</p>
     </ChakraProvider>
   );
 }
